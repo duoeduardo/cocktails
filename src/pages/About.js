21fedebcb39f52f37852862872112b0c51027cb1
Eduardo <duoeduardo@gmail.com>
2020-12-28 23:53:52 -0300

    paginas facil error-about

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, deleniti. Dolorum, a praesentium. Libero minus molestias vitae id. Voluptas quae dolorum, voluptatum necessitatibus sapiente incidunt et nulla maiores reprehenderit dolore? Rem dolore sit illum quas dicta sapiente adipisci minus laboriosam optio, nobis dolores reprehenderit beatae iure veritatis eum, hic fugiat?
+      </p>
+    </section>
   )
 }
 
